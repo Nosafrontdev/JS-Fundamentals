870f110f455029bccf37229dc863b95ac54c3b06
Nosafrontdev <nosyblink@gmail.com>
2025-07-12 15:26:14 +0100

    measuring number of argv in terminal new

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,11 +1,14 @@
-import process from 'node:process';
 
-const name = process.argv[2];
+const arg_Name = process.argv.slice(2);
 
-if (name.lenght ===0) {
-    console.log("No argument");}
-else if (name.lenght ===1) {
-    console.log("Argument found")}
+if (arg_Name.length === 0) {
+    console.log("No argument");
+}
+else if (arg_Name.length === 1) {
+    console.log("Argument found");
+}
+
     else{
-        console.log("Arguments found");}
+        console.log("Arguments found");
+    }
     
