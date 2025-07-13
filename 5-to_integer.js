602097b74020ce6d18e9dcbb5b83b5dd0f76d72f
Nosafrontdev <nosyblink@gmail.com>
2025-07-13 14:37:35 +0100

    removed space before the colon

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ const arg = process.argv[2];
 const num = Number(arg);
 
 if (!isNaN(num)) {
-    console.log(`My number : ${Math.trunc(num)}`);
+    console.log(`My number: ${Math.trunc(num)}`);
 } else {
     console.log("Not a number");
 }
