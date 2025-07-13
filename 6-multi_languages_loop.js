f408e11b56bdd15861590478382ccedcecb96d53
Nosafrontdev <nosyblink@gmail.com>
2025-07-13 14:54:18 +0100

    created for loops

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+const numb = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+function num (numb) {
+    for(let i = 0; i < numb.length; i++){
+        console.log(numb[i]);
+    }
+
+}
+
+num(numb);
